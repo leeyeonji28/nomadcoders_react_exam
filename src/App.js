c1678c8e0b50035199bb7d3d20801daea7916098
leeyeonji28 <yeon102800@gmail.com>
2022-07-28 09:38:05 +0900

    COUNTER BUTTON 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcom back!</h1>
-      <Button text={"Countinue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
