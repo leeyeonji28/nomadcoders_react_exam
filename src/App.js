ff3f0653928b894a480170727968ef2b7f533995
leeyeonji28 <yeon102800@gmail.com>
2022-07-28 11:19:25 +0900

    useEffect - 특정 부분만이 변화했을 때 원하는 코드 실행2

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
+
   useEffect(() => { // 익명함수
-    console.log("CALL THE API..."); //단 한번만 실행하고 싶은 코드
+    console.log("i run only once"); //단 한번만 실행하고 싶은 코드
   }, []);
   useEffect(() => {
-    if(keyword != "" && keyword.length > 5){ // keyword가 빈 값이 아니라면 실행
-      console.log("SEARCH FOR", keyword);
-    }
+    console.log("I run when 'keyword' changes");
   }, [keyword]); //keyword가 변화할 때 코드를 실행할 것임!! counter가 변화할 땐 실행 안됨
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]); //counter가 변화할 때 코드를 실행할 것임!!
+  useEffect(() => {
+    console.log("I run when 'counter & counter' changes");
+  }, [keyword, counter]); //keyword, counter가 변화할 때 코드를 실행할 것임!!
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
