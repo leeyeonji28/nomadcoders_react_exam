38635a721d8a25e908362f28a14bd368630ed6c8
leeyeonji28 <yeon102800@gmail.com>
2022-07-28 22:53:14 +0900

    To Do List part Two

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ function App() {
   const onChange = (event) => setToDO(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo === ""){
+    if (toDo === "") {
       return; // 아무것도 적지 않으면 함수를 실행 시키지 않음
     }
+
+    setToDos((currentArray) => [toDo, ...currentArray]); //새로운 state와 기존의 state를 더해서 새로운 array를 만들어 리턴해줌
     setToDO(""); // input값을 비워줌
-    setToDos(currentArray => [toDo, ...currentArray]); //새로운 state와 기존의 state를 더해서 새로운 array를 만들어 리턴해줌
   };
+  console.log(toDos);
   return (
     <div>
       <h1>My To toDos ({toDos.length})</h1>
@@ -23,7 +25,13 @@ function App() {
           placeholder="Write your to do..."
         />
         <button>Add To Do</button>
-      </form> 
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
