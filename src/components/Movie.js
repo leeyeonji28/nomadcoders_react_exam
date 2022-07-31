706ef3fbe18d950fba8b0911bfd92e547ed76361
leeyeonji28 <yeon102800@gmail.com>
2022-07-31 23:44:58 +0900

    Movie App part Two

---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+function Movie({ CoverImage, title, summary, genres }) {
+  return (
+    <div>
+      <img src={CoverImage} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.prototype = {
+  CoverImage: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default Movie;
