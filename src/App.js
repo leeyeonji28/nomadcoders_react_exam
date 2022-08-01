1e90703db646ccb87fe3e71c206a4ad9f3669659
leeyeonji28 <yeon102800@gmail.com>
2022-08-01 11:19:41 +0900

    deploy 2

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
+        <Route path={`${process.env.PUBLIC_URL}`} element={<Home />} />
       </Routes>
     </Router>
   );
